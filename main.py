ea0bcc00bcae879f7fe9b654fc57a1c7ac819adc
Meepyster <iforlemu@gmail.com>
2025-07-24 05:06:51 -0400

    cors added

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 # main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"hello": "from deployed app!"}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
